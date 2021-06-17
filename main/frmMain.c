bcec7b67bed128d65a30810316fd420b4da996f9
ico <it@picatek.dk>
2021-06-17 18:27:43 +0200

    upgrade to V8 in progress can compile and partially render but lv_task_handler() never returns from the first call

--- main/frmMain.c
+++ main/frmMain.c
@@ -20,36 +20,53 @@
 //#include "lv_drivers/win32drv/win32drv.h"
 
 
+static void event_handler(lv_event_t * e)
+{
+    lv_obj_t * obj = lv_event_get_target(e);
+    LV_LOG_USER("Button %d clicked", lv_obj_get_child_id(obj));
+}
+
+
 void frmMain(void)
 {
 
 
-	printf("\n=============================Hello");
+	printf("\n============================= 1");
 	fflush(stdout);
 
 	/*Create a window*/
-	    lv_obj_t * win = lv_win_create(lv_scr_act(), NULL);
-	    lv_win_set_title(win, "Window title");                        /*Set the title*/
+	  lv_obj_t * win = lv_win_create(lv_scr_act(), 40);
+
+	        lv_obj_t * btn;
+	        btn = lv_win_add_btn(win, LV_SYMBOL_LIST, 40);
+	        lv_obj_add_event_cb(btn, event_handler, LV_EVENT_CLICKED, NULL);
+
+	        lv_win_add_title(win, "A title");
+
+	        btn = lv_win_add_btn(win, LV_SYMBOL_BARS, 40);
+	        lv_obj_add_event_cb(btn, event_handler, LV_EVENT_CLICKED, NULL);
+
+	        btn = lv_win_add_btn(win, LV_SYMBOL_CLOSE, 60);
+	        lv_obj_add_event_cb(btn, event_handler, LV_EVENT_CLICKED, NULL);
+
+	        lv_obj_t * cont = lv_win_get_content(win);  /*Content can be aded here*/
+	        lv_obj_t * label =  lv_label_create(cont);
+	        lv_label_set_text(label, "This is\n"
+	                                 "a pretty\n"
+	                                 "long text\n"
+	                                 "to see how\n"
+	                                 "the window\n"
+	                                 "becomes\n"
+	                                 "scrollable.\n"
+	                                 "\n"
+	                                 "\n"
+	                                 "Some more\n"
+	                                 "text to be\n"
+	                                 "sure it\n"
+	                                 "overflows. :)");
 
 
-	    /*Add control button to the header*/
-	    lv_obj_t * close_btn = lv_win_add_btn(win, LV_SYMBOL_CLOSE);           /*Add close button and use built-in close action*/
-	    lv_obj_set_event_cb(close_btn, lv_win_close_event_cb);
-	    lv_win_add_btn(win, LV_SYMBOL_SETTINGS);        /*Add a setup button*/
-
-	    /*Add some dummy content*/
-	    lv_obj_t * txt = lv_label_create(win, NULL);
-	    lv_label_set_text(txt, "This is the content of the window\n\n"
-	                           "You can add control buttons to\n"
-	                           "the window header\n\n"
-	                           "The content area becomes\n"
-	                           "automatically scrollable is it's \n"
-	                           "large enough.\n\n"
-	                           " You can scroll the content\n"
-	                           "See the scroll bar on the right!");
-
-
-
-
+	        printf("\n============================= 2");
+	        	fflush(stdout);
 
 }
--- main/main.c
+++ main/main.c
@@ -20,6 +20,7 @@
 #include "esp_system.h"
 #include "driver/gpio.h"
 
+
 /* Littlevgl specific */
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
 #include "lvgl.h"
@@ -29,22 +30,23 @@
 
 
 #include "lvgl_helpers.h"
-#include "lv_examples/src/lv_demo_printer/lv_demo_printer.h"
 
-#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-        //#include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
-
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        #include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        #include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
-    #else
-        #error "No demo application selected."
-    #endif
-#endif
+//#include "lv_examples/src/lv_demo_printer/lv_demo_printer.h"
+//
+//#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+//    #if defined CONFIG_LV_USE_DEMO_WIDGETS
+//        //#include "lv_examples/src/lv_demo_widgets/lv_demo_widgets.h"
+//
+//    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+//        #include "lv_examples/src/lv_demo_keypad_encoder/lv_demo_keypad_encoder.h"
+//    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+//        #include "lv_examples/src/lv_demo_benchmark/lv_demo_benchmark.h"
+//    #elif defined CONFIG_LV_USE_DEMO_STRESS
+//        #include "lv_examples/src/lv_demo_stress/lv_demo_stress.h"
+//    #else
+//        #error "No demo application selected."
+//    #endif
+//#endif
 
 
 extern void frmMain(void);
@@ -88,6 +90,9 @@ static void guiTask(void *pvParameter) {
     lv_init();
 
 
+    /*Initialize the HAL (display, input devices, tick) for LVGL*/
+
+
     (void) pvParameter;
 
 
@@ -106,24 +111,24 @@ static void guiTask(void *pvParameter) {
     static lv_color_t *buf2 = NULL;
 #endif
 
-    static lv_disp_buf_t disp_buf;
+    static lv_disp_draw_buf_t disp_buf;
 
     uint32_t size_in_px = DISP_BUF_SIZE;
+
 
 #if defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_IL3820         \
     || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_JD79653A    \
     || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_UC8151D     \
     || defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_SSD1306
-
     /* Actual size in pixels, not bytes. */
     size_in_px *= 8;
 #endif
 
     /* Initialize the working buffer depending on the selected display.
      * NOTE: buf2 == NULL when using monochrome displays. */
-    lv_disp_buf_init(&disp_buf, buf1, buf2, size_in_px);
+    lv_disp_draw_buf_init(&disp_buf, buf1, buf2, size_in_px);
 
-    lv_disp_drv_t disp_drv;
+    static lv_disp_drv_t disp_drv;
     lv_disp_drv_init(&disp_drv);
     disp_drv.flush_cb = disp_driver_flush;
 
@@ -135,12 +140,13 @@ static void guiTask(void *pvParameter) {
     disp_drv.set_px_cb = disp_driver_set_px;
 #endif
 
-    disp_drv.buffer = &disp_buf;
+    //disp_drv.buffer = &disp_buf;
+    disp_drv.draw_buf=&disp_buf;
     lv_disp_drv_register(&disp_drv);
 
     /* Register an input device when enabled on the menuconfig */
 #if CONFIG_LV_TOUCH_CONTROLLER != TOUCH_CONTROLLER_NONE
-    lv_indev_drv_t indev_drv;
+   static  lv_indev_drv_t indev_drv;
     lv_indev_drv_init(&indev_drv);
     indev_drv.read_cb = touch_driver_read;
     indev_drv.type = LV_INDEV_TYPE_POINTER;
@@ -157,69 +163,78 @@ static void guiTask(void *pvParameter) {
     ESP_ERROR_CHECK(esp_timer_start_periodic(periodic_timer, LV_TICK_PERIOD_MS * 1000));
 
     /* Create the demo application */
-    create_demo_application();
-
+   // create_demo_application();
+    //frmMain();
+    printf("\n*\n");
     while (1) {
         /* Delay 1 tick (assumes FreeRTOS tick is 10ms */
-        vTaskDelay(pdMS_TO_TICKS(10));
 
+        printf("*");
+        fflush(stdout);
         /* Try to take the semaphore, call lvgl related function on success */
         if (pdTRUE == xSemaphoreTake(xGuiSemaphore, portMAX_DELAY)) {
             lv_task_handler();
+            printf("-");
             xSemaphoreGive(xGuiSemaphore);
        }
+        vTaskDelay(pdMS_TO_TICKS(10));
     }
 
-    /* A task should NEVER return */
-    free(buf1);
-#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
-    free(buf2);
-#endif
-    vTaskDelete(NULL);
+//    /* A task should NEVER return */
+//    free(buf1);
+//#ifndef CONFIG_LV_TFT_DISPLAY_MONOCHROME
+//    free(buf2);
+//#endif
+  //  vTaskDelete(NULL);
 }
-
-static void create_demo_application(void)
-{
-    /* When using a monochrome display we only show "Hello World" centered on the
-     * screen */
-#if defined CONFIG_LV_TFT_DISPLAY_MONOCHROME || \
-    defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_ST7735S
-
-    /* use a pretty small demo for monochrome displays */
-    /* Get the current screen  */
-    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
-
-    /*Create a Label on the currently active screen*/
-    lv_obj_t * label1 =  lv_label_create(scr, NULL);
-
-    /*Modify the Label's text*/
-    lv_label_set_text(label1, "Hello\nworld");
-
-    /* Align the Label to the center
-     * NULL means align on parent (which is the screen now)
-     * 0, 0 at the end means an x, y offset after alignment*/
-    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
-#else
-    /* Otherwise we show the selected demo */
-
-    #if defined CONFIG_LV_USE_DEMO_WIDGETS
-    	frmMain();
-       // lv_demo_widgets();
-        //lv_demo_printer();
-    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
-        lv_demo_keypad_encoder();
-    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
-        lv_demo_benchmark();
-    #elif defined CONFIG_LV_USE_DEMO_STRESS
-        lv_demo_stress();
-    #else
-        #error "No demo application selected."
-    #endif
-#endif
-}
+//
+//static void create_demo_application(void)
+//{
+//    /* When using a monochrome display we only show "Hello World" centered on the
+//     * screen */
+/**
+//#if defined CONFIG_LV_TFT_DISPLAY_MONOCHROME || \
+//    defined CONFIG_LV_TFT_DISPLAY_CONTROLLER_ST7735S
+ *
+ */
+//
+//    /* use a pretty small demo for monochrome displays */
+//    /* Get the current screen  */
+//    lv_obj_t * scr = lv_disp_get_scr_act(NULL);
+//
+//    /*Create a Label on the currently active screen*/
+//    lv_obj_t * label1 =  lv_label_create(scr, NULL);
+//
+//    /*Modify the Label's text*/
+//    lv_label_set_text(label1, "Hello\nworld");
+//
+//    /* Align the Label to the center
+//     * NULL means align on parent (which is the screen now)
+//     * 0, 0 at the end means an x, y offset after alignment*/
+//    lv_obj_align(label1, NULL, LV_ALIGN_CENTER, 0, 0);
+//#else
+//    /* Otherwise we show the selected demo */
+//
+//    #if defined CONFIG_LV_USE_DEMO_WIDGETS
+//    	frmMain();
+//       // lv_demo_widgets();
+//        //lv_demo_printer();
+//    #elif defined CONFIG_LV_USE_DEMO_KEYPAD_AND_ENCODER
+//        lv_demo_keypad_encoder();
+//    #elif defined CONFIG_LV_USE_DEMO_BENCHMARK
+//        lv_demo_benchmark();
+//    #elif defined CONFIG_LV_USE_DEMO_STRESS
+//        lv_demo_stress();
+//    #else
+//        #error "No demo application selected."
+//    #endif
+//#endif
+//
+//
+//}
 
 static void lv_tick_task(void *arg) {
     (void) arg;
-
+    //printf(".");
     lv_tick_inc(LV_TICK_PERIOD_MS);
 }
