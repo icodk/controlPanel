46e7a48ab75c82cfa0954d883c5833ee57349a3e
ico <it@picatek.dk>
2021-08-20 14:29:13 +0200

    adde files

---
+++ main/ethernet_cfg.c
@@ -0,0 +1,28 @@
+/*
+ * ethernet_cfg.c
+ *
+ *  Created on: 19. aug. 2021
+ *      Author: ico
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lvgl/lvgl.h"
+#include "dataStorage.h"
+#include "ui_common.h"
+#include "text_table.h"
+
+
+
+
+
+void ethernet_cfg_init(void) {
+
+
+}
+
+bool isEthernetSupported(void) {
+    return true;
+}
+
---
+++ main/wifi_cfg.c
@@ -0,0 +1,24 @@
+﻿
+/*
+ * wifi_cfg.c
+ *
+ *  Created on: 19. aug. 2021
+ *      Author: ico
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lvgl/lvgl.h"
+#include "dataStorage.h"
+#include "ui_common.h"
+#include "text_table.h"
+
+bool isWifisupported(void) {
+    return false;
+}
+
+void wifi_cfg_init(void) {
+
+
+}
